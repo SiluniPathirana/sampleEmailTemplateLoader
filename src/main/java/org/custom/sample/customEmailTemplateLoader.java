9d590d4a9e3eb575861fa89c66ad0e663b29c21e
SiluniPathirana <siluni@wso2.com>
2018-11-09 13:45:11 +0530

    sample email template loader

---
+++ src/main/java/org/custom/sample/customEmailTemplateLoader.java
@@ -0,0 +1,48 @@
+package org.custom.sample;
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.email.mgt.EmailTemplateManagerImpl;
+import org.wso2.carbon.email.mgt.exceptions.I18nEmailMgtException;
+import org.wso2.carbon.utils.AbstractAxis2ConfigurationContextObserver;
+import org.wso2.carbon.email.mgt.EmailTemplateManager;
+
+public class customEmailTemplateLoader extends AbstractAxis2ConfigurationContextObserver {
+
+    private static final Log log = LogFactory.getLog(customEmailTemplateLoader.class);
+
+    @Override
+    public void createdConfigurationContext(ConfigurationContext configContext) {
+
+
+        log.info("-------------------------Tenant Loading ---------------------");
+        String tenantDomain = PrivilegedCarbonContext.getThreadLocalCarbonContext().getTenantDomain(true);
+        EmailTemplateManager templateManager = new EmailTemplateManagerImpl();
+        try {
+            templateManager.addDefaultEmailTemplates(tenantDomain);
+        } catch (I18nEmailMgtException e) {
+            String message = "Error occurred while loading default email templates for the tenant : " + tenantDomain;
+            log.error(message);
+        }
+    }
+
+}
